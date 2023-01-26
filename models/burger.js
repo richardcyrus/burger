2dfd7df47a243ea3068f77fc0ad18df10dbd0911
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-01-25 19:43:23 -0500

    Update dependencies and Node.js version

--- models/burger.js
+++ models/burger.js
@@ -5,48 +5,48 @@
  * (c) 2019 Richard Cyrus <richard.cyrus@rcyrus.com>
  */
 
-const db = require('../config/orm');
-const table = 'burgers';
+const db = require('../config/orm')
+const table = 'burgers'
 
 const burger = {
-    /**
-     * Retrieve all records from the database and send the results to
-     * the `callback`.
-     *
-     * @param callback
-     */
-    all: (callback) => {
-        db.selectAll(table, (res) => callback(res));
-    },
+  /**
+   * Retrieve all records from the database and send the results to
+   * the `callback`.
+   *
+   * @param callback
+   */
+  all: (callback) => {
+    db.selectAll(table, (res) => callback(res))
+  },
 
-    /**
-     * Create a new record in the database, and pass any status to the
-     * `callback`
-     *
-     * @param value
-     * @param callback
-     */
-    create: (value, callback) => {
-        db.insertOne(table, 'burger_name', value, (res) => callback(res));
-    },
+  /**
+   * Create a new record in the database, and pass any status to the
+   * `callback`
+   *
+   * @param value
+   * @param callback
+   */
+  create: (value, callback) => {
+    db.insertOne(table, 'burger_name', value, (res) => callback(res))
+  },
 
-    /**
-     * Update a record in the database, and pass any status to the
-     * `callback`
-     *
-     * @param value
-     * @param callback
-     */
-    update: (value, callback) => {
-        const values = {
-            'devoured': true,
-        };
-        const criteria = {
-            'id': value,
-        };
+  /**
+   * Update a record in the database, and pass any status to the
+   * `callback`
+   *
+   * @param value
+   * @param callback
+   */
+  update: (value, callback) => {
+    const values = {
+      devoured: true,
+    }
+    const criteria = {
+      id: value,
+    }
 
-        db.updateOne(table, values, criteria, (res) => callback(res));
-    },
-};
+    db.updateOne(table, values, criteria, (res) => callback(res))
+  },
+}
 
-module.exports = burger;
+module.exports = burger
