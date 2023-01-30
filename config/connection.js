77eec3c17eba58e48d743639a5964935a110efd8
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-01-29 22:29:56 -0500

    Remove invalid pool parameters

--- config/connection.js
+++ config/connection.js
@@ -13,8 +13,6 @@ if (process.env.MYSQL_URL) {
       uri: process.env.MYSQL_URL,
       waitForConnections: true,
       connectionLimit: 10,
-      maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-      idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
       queueLimit: 0
   })
 } else {
@@ -23,8 +21,6 @@ if (process.env.MYSQL_URL) {
       uri: process.env.MYSQL_URL,
       waitForConnections: true,
       connectionLimit: 10,
-      maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-      idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
       queueLimit: 0
   })
 }
